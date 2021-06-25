71770b9e33747c9d6969bb8cb0374e44fabd198c
Shreya <64486266+shreyacasm@users.noreply.github.com>
2021-06-25 21:31:01 +0530

    Time: 604 ms (8.82%), Space: 273.4 MB (5.07%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -8,13 +8,10 @@ public:
         for(int i=0;i<s.length();i++){
             int count=1;
             for(int j=i+1;j<s.length();j++){
-                string str = s.substr(i,j-i);
-                int found = str.find(s[j]);
                 //cout<<"here str is : "<<str<<" and we are finding "<<s[j]<<"\n";
-                if(found!=string::npos){
-                   int temp = j-i;
-                    if(temp>max)
-                        max = temp;
+                if(((s.substr(i,j-i)).find(s[j]))!=string::npos){
+                    if((j-i)>max)
+                        max = j-i;
                     //cout<<"breaking after "<<str<<"where i is"<<i<<"\n";
                     break;
                 }
