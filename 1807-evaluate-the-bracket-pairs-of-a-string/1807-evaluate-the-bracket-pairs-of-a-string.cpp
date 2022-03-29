e884aa4b4b67ea693df20a747e99cd85de1b86b1
Shreya <64486266+shreyacasm@users.noreply.github.com>
2022-03-29 14:22:05 +0530

    Time: 696 ms (25.57%), Space: 115.4 MB (89.61%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,62 +1,24 @@
 class Solution {
 public:
-    string evaluate(string s, vector<vector<string>>& k) {
-        /*map<string,string> mp;
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        map<string,string> mp;
         for(int i=0;i<knowledge.size();i++){
             mp[knowledge[i][0]]=knowledge[i][1];
         }
         string ans;
-        for(int i=0;i<s.size();i++){
+         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
-                string req="";
-                i++;
-                while(s[i]!=')'){
-                    req=req+s[i];
-                    i++;
-                }
-                if(mp.find(req)!=mp.end()){
-                    ans=ans+mp[req];
+                if(mp.find(s.substr(i+1,(s.find(')',i))-i-1))!=mp.end()){
+                    ans+=mp[s.substr(i+1,(s.find(')',i))-i-1)];
                 }
                 else{
-                    ans=ans+'?';
+                    ans+='?';
                 }
-                //cout<<"here ans is "<<ans<<"\n";
+                i=s.find(')',i);
             }
             else{
-                ans=ans+s[i];
-            }
-        }
-        return ans;*/
-        string ans;
-        map<string,string> m;
-        for(int i=0;i<k.size();i++)
-        {
-            m[k[i][0]]=k[i][1];
-        }
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(')
-            {
-                string tp;
-                i++;
-                while(s[i]!=')')
-                {
-                    tp+=s[i];
-                    i++;
-                }
-                int f=0;
-                if(m.find(tp)!=m.end())
-                {
-                    ans+=m[tp];
-                }
-                else
-                    ans+='?';
-            }
-            else
-            {
                 ans+=s[i];
             }
-            
         }
         return ans;
     }
