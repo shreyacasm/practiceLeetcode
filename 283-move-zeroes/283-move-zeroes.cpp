63444de39bd30b190661304cf03733ba88025e39
Shreya <64486266+shreyacasm@users.noreply.github.com>
2022-07-18 00:16:07 +0530

    Time: 180 ms (9.81%), Space: 19.3 MB (13.43%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int len=nums.size();
-        // vector<int>::iterator it;
-        // for(auto it=nums.begin();it!=nums.end() && len--;){
-        //     if(*it==0){
-        //         nums.erase(it);
-        //         nums.push_back(0);
-        //     }
-        //     else if(it!=nums.end()){
-        //         ++it;
-        //     }
-        // }
-        int j=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                nums[j++]=nums[i];
+        int count=0;
+        for(auto i=nums.begin();i!=nums.end();i++){
+            if(*i==0){
+                nums.erase(i);
+                nums.push_back(0);
+                i--;
             }
-            
-        }
-        j=nums.size()-j;
-        while(j){
-            nums[nums.size()-j]=0;
-            j--;
+            count++;
+            if(count>nums.size())
+                break;
         }
     }
 };
