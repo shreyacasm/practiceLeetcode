4b5dcfbcdf927bc93c8d7bec8be7b7bfc8f6cc43
Shreya <64486266+shreyacasm@users.noreply.github.com>
2022-07-19 00:24:42 +0530

    Time: 110 ms (38.56%), Space: 10.1 MB (95.63%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>::iterator it;
+        vector<int> res;
+        for(auto ir=nums.begin(); ir != nums.end(); ++ir){
+            
+            it = std::find (ir+1, nums.end(), target-*ir);
+            if (it != nums.end()){
+                res.push_back(ir-nums.begin());
+                res.push_back(it-nums.begin());
+                break;
+                
+            }
+        }
+        return res;
+    }
+};
